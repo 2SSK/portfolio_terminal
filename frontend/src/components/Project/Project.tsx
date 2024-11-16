b807edb07cfeb0d080e1cf2303be755df09bf0a4
SSK <100ravsinghkarmwar@gmail.com>
2024-11-16 08:30:22 +0530

    refactor: updated the project list on the website

--- frontend/src/components/Project/Project.tsx
+++ frontend/src/components/Project/Project.tsx
@@ -19,6 +19,11 @@ export default function Project() {
         description="A responsive university website built with React.js"
       />
       <Link
+        href="https://gemini-clone-ssk.vercel.app/"
+        title="GeminiAI"
+        description="A clone of GeminiAI website built with React.js"
+      />
+      <Link
         href="https://github.com/2SSK/dot-files/"
         title="i3wm Dotfiles"
         description="A complete setup for i3wm with configurations for Alacritty, Kitty, Neovim, Emacs, Polybar, Rofi, and more."
@@ -27,16 +32,6 @@ export default function Project() {
         href="https://github.com/2SSK/go-todo-cli-app"
         title="Go Todo CLI"
         description="A simple CLI app to manage todos built with Go"
-      />
-      <Link
-        href="https://gemini-clone-ssk.vercel.app/"
-        title="GeminiAI"
-        description="A clone of GeminiAI website built with React.js"
-      />
-      <Link
-        href="https://mern-todo-app-ssk.vercel.app/"
-        title="MERN Todo App"
-        description="A simple todo app built with MERN stack"
       />
     </div>
   );
