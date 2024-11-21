c42adf4d9ea5d06b8521618149c83505024c443a
SSK <100ravsinghkarmwar@gmail.com>
2024-11-21 09:34:45 +0530

    fix : Added .gitignore file to protect uri's

--- backend/db/connection.go
+++ backend/db/connection.go
@@ -1,9 +1,11 @@
 package db
 
 import (
-	"github.com/2SSK/portfolio_terminal/models"
+	"github.com/2SSK/portfolio_terminal/backend/models"
 	"log"
 
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mongodb"
 	"gorm.io/gorm"
 )
+
+var DB *gorm.DB
