e9c96e73205bc53b55733274cdb4d84e663e05c9
SSK <100ravsinghkarmwar@gmail.com>
2024-11-21 14:22:00 +0530

    refactor: removed backend (will rewrite again)

--- backend/models/skills.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Skill struct {
-	ID   uint   `json:"id" gorm:"primary_key"`
-	Name string `json:"name"`
-	Type string `json:"type"`
-}
--- backend/models/socials.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Social struct {
-	ID         uint   `json:"id" gorm:"primary_key"`
-	Title      string `json:"title"`
-	ShowLink   string `json:"show_link"`
-	ActualLink string `json:"actual_link"`
-}
