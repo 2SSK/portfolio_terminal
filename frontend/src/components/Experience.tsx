d274643ae53211eb3a27a535fa1eb95d26ed350f
2SSK <100ravsinghkarmwar@gmail.com>
2025-03-06 08:46:27 +0530

    updated experience

--- frontend/src/components/Experience.tsx
+++ frontend/src/components/Experience.tsx
@@ -1,13 +1,22 @@
 const workExperience = [
   {
-    company: "EOXS",
-    companyLink: "https://www.eoxs.com",
-    position: "Software Developer Intern",
-    startDate: "03/03/2025",
+    company: "DEMO",
+    companyLink: "https://ssk-portfolio.vercel.app",
+    position: "Software Developer",
+    startDate: "Mar 2025",
     endDate: "Ongoing",
     description:
       "Responsible for writing clean, efficient code, designing software architecture, debugging, and troubleshooting. Collaborates with teams to develop secure and scalable applications, ensuring best practices in testing, documentation, and deployment. Experienced in CI/CD, secure coding, and data protection.",
   },
+  //{
+  //  company: "EOXS",
+  //  companyLink: "https://www.eoxs.com",
+  //  position: "Software Developer Intern",
+  //  startDate: "03/03/2025",
+  //  endDate: "Ongoing",
+  //  description:
+  //    "Responsible for writing clean, efficient code, designing software architecture, debugging, and troubleshooting. Collaborates with teams to develop secure and scalable applications, ensuring best practices in testing, documentation, and deployment. Experienced in CI/CD, secure coding, and data protection.",
+  //},
 ];
 
 const Experience = () => {
