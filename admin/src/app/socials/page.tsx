7aef065d17c5f80b602f16d2f87ea108b7052378
2SSK <100ravsinghkarmwar@gmail.com>
2025-03-28 06:13:11 +0530

    feat: add new pages for Experience, Projects, Resume, Socials, and Tools
    - Introduce ExperiencePage, ProjectPage, ResumePage, SocialPage, and ToolsPage components.
    - Each page includes a simple layout with a header for easy navigation within the admin panel.

---
+++ admin/src/app/socials/page.tsx
@@ -0,0 +1,7 @@
+export default function SocialPage() {
+  return (
+    <div>
+      <h1>social page</h1>
+    </div>
+  );
+}
---
+++ admin/src/app/tools/page.tsx
@@ -0,0 +1,7 @@
+export default function ToolsPage() {
+  return (
+    <div>
+      <h1>tool page</h1>
+    </div>
+  );
+}
